2117d69aa46687c1c4ef0c02ca904827d91b4015
DAINAWAMBUI <61407935+DAINAWAMBUI@users.noreply.github.com>
2022-11-08 12:26:54 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<div>
+<tr><td>email:<input type="text" name="email"required></td></tr>
+<br>
+<br>
+<tr><td>password:<input type="password" name="password" required></td></tr>
+<br><br>						<tr><td colspan="2"><input type="submit" name="login" value="LOGIN"></td></tr>
+
+<div>
+	
+
+</div>	
+
+
+
+</div>
